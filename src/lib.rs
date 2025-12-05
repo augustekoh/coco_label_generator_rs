a23ff1c5062c1e0201cdbdd29a6597a184f7d65a
augustekoh <54816308+augustekoh@users.noreply.github.com>
2025-12-04 20:47:48 -0500

    Update
    SYMREF: [main]

--- src/lib.rs
+++ src/lib.rs
@@ -166,7 +166,7 @@ fn generate_json(scenes: Vec<Scene>) {
     let metadata = Arc::new(Mutex::new(vec![]));
     let bar = ProgressBar::new(scenes.len().try_into().unwrap());
     bar.set_style(ProgressStyle::with_template(
-        "{spinner} {wide_bar} ({percent}) [{per_sec} {elapsed}:{eta}]").unwrap());
+        "{spinner} {wide_bar} ({percent}) [{per_sec:2} {elapsed}:{eta}]").unwrap());
     bar.enable_steady_tick(std::time::Duration::from_millis(50));
     scenes
         .par_iter().panic_fuse()
