62667986934abbd744c7ca5f5a7ff7a8eb6028c1
augustekoh <54816308+augustekoh@users.noreply.github.com>
2025-12-06 17:13:55 -0500

    Add tests
    SYMREF: [main]

--- src/lib.rs
+++ src/lib.rs
@@ -406,7 +406,7 @@ impl ViewMetadataSerde {
     }
 }
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, Eq, PartialEq)]
 #[serde(into = "Bboxx1y1x2y2Serde")]
 struct Bboxx1y1x2y2 {
     x1: usize,
@@ -484,7 +484,7 @@ fn derive_view_metadata(scene: &Scene, view_metadata: Arc<Mutex<Vec<ViewMetadata
             ann_builder.instance_id.replace(*id);
             ann_builder.bbox.replace(bbox);
             ann_builder.category_id.replace(FOREGROUND_CATEGORY_ID);
-            ann_builder.area.replace(*areas.get(&inst_obj_val).unwrap());
+            ann_builder.area.replace(areas[&inst_obj_val]);
             visible_map.insert(*id, ann_builder.build());
         }
         check_count_in_csv(
@@ -565,4 +565,74 @@ fn areas(arr: &Array2<InstancesObjectsValue>) -> HashMap<InstancesObjectsValue, 
         areas.insert(k, v.into());
     }
     areas
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn small_areas() {
+        let b_ = InstancesObjectsValue::Background;
+        let f1 = InstancesObjectsValue::Object(InstanceId::new(1));
+        let f2 = InstancesObjectsValue::Object(InstanceId::new(2));
+        let f3 = InstancesObjectsValue::Object(InstanceId::new(3));
+        let arr: Array2<InstancesObjectsValue> = ndarray::array![
+            [b_, f1, f1, f1, f2, f1, f2],
+            [b_, f1, f1, f1, f2, b_, b_],
+            [f3, f1, f2, f3, f2, f1, f2],
+            [b_, f3, f3, f3, f1, f1, f1],
+        ];
+        let areas = areas(&arr);
+        assert_eq!(areas[&f1], Area::new(12.0));
+        assert_eq!(areas[&f2], Area::new(6.0));
+        assert_eq!(areas[&f3], Area::new(5.0));
+        assert_eq!(areas[&b_], Area::new(5.0));
+
+        assert_eq!(bounding_box(&arr, &f1),
+                   Bboxx1y1x2y2::bulider().set_x1(1).set_x2(7).set_y1(0).set_y2(4).build().unwrap());
+        assert_eq!(bounding_box(&arr, &f2),
+                   Bboxx1y1x2y2::bulider().set_x1(2).set_x2(7).set_y1(0).set_y2(3).build().unwrap());
+        assert_eq!(bounding_box(&arr, &f3),
+                   Bboxx1y1x2y2::bulider().set_x1(0).set_x2(4).set_y1(2).set_y2(4).build().unwrap());
+        assert_eq!(bounding_box(&arr, &b_),
+                   Bboxx1y1x2y2::bulider().set_x1(0).set_x2(7).set_y1(0).set_y2(4).build().unwrap());
+    }
+
+    #[test]
+    fn nano_areas() {
+        let b_ = InstancesObjectsValue::Background;
+        let f1 = InstancesObjectsValue::Object(InstanceId::new(1));
+        let f2 = InstancesObjectsValue::Object(InstanceId::new(2));
+        let f3 = InstancesObjectsValue::Object(InstanceId::new(3));
+        let f4 = InstancesObjectsValue::Object(InstanceId::new(4));
+        let f5 = InstancesObjectsValue::Object(InstanceId::new(5));
+        let arr: Array2<InstancesObjectsValue> = ndarray::array![
+            [b_, b_, b_, b_, f2, b_, b_],
+            [b_, f1, b_, b_, f2, b_, b_],
+            [b_, b_, b_, f3, b_, f4, b_],
+            [b_, b_, f3, b_, b_, f4, f5],
+        ];
+        let areas = areas(&arr);
+        assert_eq!(areas[&f1], Area::new(1.0));
+        assert_eq!(areas[&f2], Area::new(2.0));
+        assert_eq!(areas[&f3], Area::new(2.0));
+        assert_eq!(areas[&f4], Area::new(2.0));
+        assert_eq!(areas[&f5], Area::new(1.0));
+        assert_eq!(areas[&b_], Area::new(20.0));
+
+        assert_eq!(bounding_box(&arr, &f1),
+                   Bboxx1y1x2y2::bulider().set_x1(1).set_x2(2).set_y1(1).set_y2(2).build().unwrap());
+        assert_eq!(bounding_box(&arr, &f2),
+                   Bboxx1y1x2y2::bulider().set_x1(4).set_x2(5).set_y1(0).set_y2(2).build().unwrap());
+        assert_eq!(bounding_box(&arr, &f3),
+                   Bboxx1y1x2y2::bulider().set_x1(2).set_x2(4).set_y1(2).set_y2(4).build().unwrap());
+        assert_eq!(bounding_box(&arr, &f4),
+                   Bboxx1y1x2y2::bulider().set_x1(5).set_x2(6).set_y1(2).set_y2(4).build().unwrap());
+        assert_eq!(bounding_box(&arr, &f5),
+                   Bboxx1y1x2y2::bulider().set_x1(6).set_x2(7).set_y1(3).set_y2(4).build().unwrap());
+        assert_eq!(bounding_box(&arr, &b_),
+                   Bboxx1y1x2y2::bulider().set_x1(0).set_x2(7).set_y1(0).set_y2(4).build().unwrap());
+    }
+
 }
